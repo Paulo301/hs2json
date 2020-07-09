ea48c5e8ae37ebdba80272d949ab3d5635bdd92c
Paulo301 <paulovictor1918@gmail.com>
2020-07-09 12:23:33 -0300

    Projeto antes de começar o tutorial.

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Lib
+
+import Test.QuickCheck
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
---
+++ src/Lib.hs
@@ -0,0 +1,15 @@
+module Lib
+    ( 
+				someFunc,
+        qsort
+    ) where
+
+
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort lhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"

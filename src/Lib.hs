ea7360ab536eb25bb980e65e92cb58309003991d
Paulo301 <paulovictor1918@gmail.com>
2020-07-09 13:14:50 -0300

    Representing JSON data in Haskell

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,15 @@
 module Lib
-    ( 
-				someFunc,
-        qsort
-    ) where
+ ( 
+  someFunc,
+  qsort
+ ) where
 
 
 qsort :: Ord a => [a] -> [a]
 qsort []     = []
-qsort (x:xs) = qsort lhs ++ [x] ++ qsort lhs
-    where lhs = filter  (< x) xs
-          rhs = filter (>= x) xs
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+ where lhs = filter  (< x) xs
+       rhs = filter (>= x) xs
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"

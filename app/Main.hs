62c6302c91993ef09e827214a4cd868fc1cb4ebe
Paulo301 <paulovictor1918@gmail.com>
2020-07-10 10:13:31 -0300

    Alteração na Main para realização do teste de funcionamento

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
 import SimpleJSON
+import Prettify
+import PrettyJSON
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main :: IO()
+main = do
+  let value = renderJValue (JObject [("nome", JString "Sergio Costa"), ("matricula", JNumber 123), ("status", JBool True)])
+  putStrLn(pretty 10 value)
